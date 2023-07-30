662daeb3689dc37909d8d38ed3dc50543068c061
Aladdin4u <taofikakinkumi@gmail.com>
2023-07-30 17:19:53 +0100

    initial commit

--- src/services/diaryService.ts
+++ src/services/diaryService.ts
@@ -1,36 +1,37 @@
-import diaries from '../../data/entries';
+import diaries from "../../data/entries";
 
-import { NewDiaryEntry, NonSensitiveDiaryEntry, DiaryEntry } from '../types';
+import { NewDiaryEntry, NonSensitiveDiaryEntry, DiaryEntry } from "../types";
 
 const getEntries = (): DiaryEntry[] => {
-    return diaries;
+  return diaries;
 };
 
 const getNonSensitiveEntires = (): NonSensitiveDiaryEntry[] => {
-    return diaries.map(({ id, date, weather, visibility }) => ({
-        id,
-        date,
-        weather,
-        visibility,
-    }));
-}
-const addDiary = ( entry: NewDiaryEntry ): DiaryEntry => {
-    const newDiaryEntry = { 
-        id: Math.max(...diaries.map(d => d.id)) + 1, 
-        ...entry
-    }
-    diaries.push(newDiaryEntry)
-    return newDiaryEntry;
+  return diaries.map(({ id, date, weather, visibility }) => ({
+    id,
+    date,
+    weather,
+    visibility,
+  }));
+};
+
+const addDiary = (entry: NewDiaryEntry): DiaryEntry => {
+  const newDiaryEntry = {
+    id: Math.max(...diaries.map((d) => d.id)) + 1,
+    ...entry,
+  };
+  diaries.push(newDiaryEntry);
+  return newDiaryEntry;
 };
 
 const findById = (id: number): DiaryEntry | undefined => {
-    const entry = diaries.find(d => d.id === id);
-    return entry;
-}
+  const entry = diaries.find((d) => d.id === id);
+  return entry;
+};
 
 export default {
-    getEntries,
-    getNonSensitiveEntires,
-    addDiary,
-    findById
+  getEntries,
+  getNonSensitiveEntires,
+  addDiary,
+  findById,
 };
